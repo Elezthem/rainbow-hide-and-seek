4b7fe41640eca15e6f8604aa3c8e0c1b2663ad27
ElezthemDev <97851451+Elezthem@users.noreply.github.com>
2024-04-06 21:43:33 +0300

    Add files via upload

---
+++ Mobile/Scripts/CanvasInputs/UICanvasControllerInput.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace StarterAssets
+{
+    public class UICanvasControllerInput : MonoBehaviour
+    {
+
+        [Header("Output")]
+        public StarterAssetsInputs starterAssetsInputs;
+        public GameObject InputManager;
+        void Start()
+        {
+            Invoke("VirtualVoid" , 1f);
+        }
+        public void VirtualMoveInput(Vector2 virtualMoveDirection)
+        {
+            starterAssetsInputs.MoveInput(virtualMoveDirection);
+        }
+
+        public void VirtualLookInput(Vector2 virtualLookDirection)
+        {
+            starterAssetsInputs.LookInput(virtualLookDirection);
+        }
+
+        public void VirtualJumpInput(bool virtualJumpState)
+        {
+            starterAssetsInputs.JumpInput(virtualJumpState);
+        }
+
+        public void VirtualSprintInput(bool virtualSprintState)
+        {
+            starterAssetsInputs.SprintInput(virtualSprintState);
+        }
+        void VirtualVoid()
+        {
+            if (Application.platform == RuntimePlatform.Android) Instantiate(InputManager);
+        }
+    }
+
+}
